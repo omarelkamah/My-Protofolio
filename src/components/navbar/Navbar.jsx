9140aec3e0db06a7003a28cbd063fe0d730a028e
omarelkamah <omarwork010@gmail.com>
2022-04-10 13:27:14 -0700

    Fix Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,29 +1,47 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './navbar.scss';
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import './navbar.scss'
 
-
-export default function Navbar({dark, openMenue, handeleMenue}) {
+export default function Navbar ({ openMenue, handeleMenue }) {
+  const theamState = useSelector(state => state)
   return (
-    <div className={'navbar ' + (openMenue && "active")} style={{background: dark && "#333"}}>
-        <div className="navbarCon">
-          <div className="left">
-            <Link to="/" className='logo'>Omar Ahmed..</Link>
-            <div className="navbar-contact" style={{color: dark && "#fff"}}>
-              <i className="far fa-envelope-open"></i>
-              <a href="mailto:omarwork010@gmail.com">omarwork010@gmail.com</a>
-            </div>
-            <div className="navbar-contact"  style={{color: dark && "#fff"}}>
-              <i className="fas fa-phone"></i>
-              <span>01006129547</span>
-            </div>
+    <div
+      className={'navbar ' + (openMenue && 'active')}
+      style={{
+        background: theamState.value && '#333',
+        color: theamState.value && '#fff'
+      }}
+    >
+      <div className='navbarCon'>
+        <div className='left'>
+          <Link to='/' className='logo'>
+            Omar Ahmed..
+          </Link>
+          <div className='navbar-contact'>
+            <i className='far fa-envelope-open'></i>
+            <a href='mailto:omarwork010@gmail.com'>omarwork010@gmail.com</a>
           </div>
-          <div className="right" onClick={() => handeleMenue(!openMenue)}>
-            <span className="one" style={{background: dark && "#fff"}}></span>
-            <span className="two" style={{background: dark && "#fff"}}></span>
-            <span className="three" style={{background: dark && "#fff"}}></span>
+          <div className='navbar-contact'>
+            <i className='fas fa-phone'></i>
+            <span>01006129547</span>
           </div>
         </div>
+        <div className='right' onClick={() => handeleMenue(!openMenue)}>
+          <span
+            className='one'
+            style={{ background: theamState.value && '#fff' }}
+          ></span>
+          <span
+            className='two'
+            style={{ background: theamState.value && '#fff' }}
+          ></span>
+          <span
+            className='three'
+            style={{ background: theamState.value && '#fff' }}
+          ></span>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
