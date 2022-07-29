9eaac55873ad84897bb6ebdcef321e01fd374bb9
omarelkamah <omarwork010@gmail.com>
2022-07-29 10:27:59 +0200

    remove form contact and add links social media

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,37 +1,17 @@
 import './contact.scss'
-import Phone from '../../img/phone.png'
-import Email from '../../img/email.png'
-import Address from '../../img/address.png'
-import { useRef, useState } from 'react'
 import Fade from 'react-reveal/Fade'
-
-import emailjs from 'emailjs-com'
 import { useSelector } from 'react-redux'
+import { BsFillTelephonePlusFill } from 'react-icons/bs'
+import { MdEmail } from 'react-icons/md'
+import { HiLocationMarker } from 'react-icons/hi'
+import {
+  AiFillLinkedin,
+  AiOutlineWhatsApp,
+  AiFillFacebook
+} from 'react-icons/ai'
 
 export default function Contact () {
-  const formRef = useRef()
-  const [done, setDone] = useState()
   const theamState = useSelector(state => state)
-
-  const handelSubmit = e => {
-    e.preventDefault()
-    emailjs
-      .sendForm(
-        'service_34ehnfo',
-        'template_l30h67z',
-        formRef.current,
-        'user_XkHowpzeoMvwEDtlXZ6D8'
-      )
-      .then(
-        result => {
-          console.log(result.text)
-          setDone(true)
-        },
-        error => {
-          console.log(error.text)
-        }
-      )
-  }
 
   return (
     <div
@@ -46,20 +26,20 @@ export default function Contact () {
         <div className='row'>
           <div className='col-lg-6 c-left'>
             <Fade left>
-              <h1 className='c-title'>Let's discuss your project</h1>
+              <h1 className='c-title'>Contact Info</h1>
               <div className='c-info'>
                 <div className='c-info-item'>
-                  <img src={Phone} alt='' className='c-icon' />
+                  <BsFillTelephonePlusFill className='c-icon' />
                   +20 101 604 629 8
                 </div>
                 <div className='c-info-item'>
-                  <img className='c-icon' src={Email} alt='' />
+                  <MdEmail className='c-icon' />
                   <a href='mailto:omarwork010@gmail.com'>
                     omarwork010@gmail.com
                   </a>
                 </div>
                 <div className='c-info-item'>
-                  <img className='c-icon' src={Address} alt='' />
+                  <HiLocationMarker className='c-icon' />
                   Alexandria - El Mandara
                 </div>
               </div>
@@ -67,38 +47,39 @@ export default function Contact () {
           </div>
           <div className='col-lg-6 c-right'>
             <Fade right>
-              <p className='c-desc'>
-                <b>What’s your story?</b> Get in touch. Always available for
-                freelancing if the right project comes along. me.
-              </p>
-              <form ref={formRef} onSubmit={handelSubmit}>
-                <input
-                  style={{ backgroundColor: theamState.value && '#333' }}
-                  type='text'
-                  placeholder='Name'
-                  name='user_name'
-                />
-                <input
-                  style={{ backgroundColor: theamState.value && '#333' }}
-                  type='text'
-                  placeholder='Subject'
-                  name='user_subject'
-                />
-                <input
-                  style={{ backgroundColor: theamState.value && '#333' }}
-                  type='text'
-                  placeholder='Email'
-                  name='user_email'
-                />
-                <textarea
-                  style={{ backgroundColor: theamState.value && '#333' }}
-                  rows='5'
-                  placeholder='Message'
-                  name='message'
-                />
-                <button>Submit</button>
-                {done && 'Thanks Alot ...'}
-              </form>
+              <h2 className='c-title'>Social Media</h2>
+              <div className='c-links'>
+                <div className='c-link'>
+                  <AiFillLinkedin className='c-icon' />
+                  <a
+                    href='https://www.linkedin.com/in/omar-el-kammah-85abb4232/'
+                    target='_blank'
+                    rel='noreferrer'
+                  >
+                    Linkedin
+                  </a>
+                </div>
+                <div className='c-link'>
+                  <AiOutlineWhatsApp className='c-icon' />
+                  <a
+                    href='https://wa.me/qr/IRWDXD7ABZCWP1'
+                    target='_blank'
+                    rel='noreferrer'
+                  >
+                    Whats App
+                  </a>
+                </div>
+                <div className='c-link'>
+                  <AiFillFacebook className='c-icon' />
+                  <a
+                    href='https://www.facebook.com/omarelkammah10/'
+                    target='_blank'
+                    rel='noreferrer'
+                  >
+                    Facebook
+                  </a>
+                </div>
+              </div>
             </Fade>
           </div>
         </div>
